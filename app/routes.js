ab396fe1968c1ddf4338fc8883c1566ee57d6ed2
mehmet <mehmet.tuna@tsoft.com.tr>
2021-01-09 23:16:14 +0300

    Delete routes.js

--- app/routes.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (app) {
-  //api ----------------------------------------
-
-  //example
-  app.get("/", function (req, res) {
-    res.send("merhaba tekrar guncellendi 11");
-  });
-};
